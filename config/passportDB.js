98da7da8f9217a7626d4fbf5fea4b7b76376e089
kirylmisiuk <kirill.misiuk@gmail.com>
2019-11-11 14:14:42 +0300

    change passport.js

--- config/passportDB.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'connection': {
-        'host': 'bofsd7plrbhrvba1cobh-mysql.services.clever-cloud.com',
-        'user': 'udl5boqszgimeawr',
-        'password': 'fbgSmoTWoUhGyNK5txe2'
-    },
-    'database': 'bofsd7plrbhrvba1cobh',
-    'users_table': 'libraries'
-};
--- js/passport.js
+++ js/passport.js
@@ -1,87 +1,140 @@
-const LocalStrategy = require('passport-local').Strategy;
-const mysql = require('mysql');
-const bcrypt = require('bcrypt-nodejs');
-const dbconfig = require('../config/passportDB');
-
-const connection = mysql.createConnection(dbconfig.connection);
-connection.query('USE ' + dbconfig.database);
-
+const bCrypt = require('bcrypt-nodejs');
+const {Library} = require('../config/sequelize');
 
 module.exports = function (passport) {
-
-
+    let LocalStrategy = require('passport-local').Strategy;
     passport.serializeUser(function (user, done) {
 
         done(null, user.id);
+
     });
-
-
     passport.deserializeUser(function (id, done) {
-        connection.query("SELECT * FROM libraries WHERE id = ? ", [id], function (err, rows) {
-            done(err, rows[0]);
-        });
-    });
-    passport.use(
-        'local-signup',
-        new LocalStrategy({
 
-                usernameField: 'name',
-                passwordField: 'password',
-                passReqToCallback: true
-            },
-            function (req, username, password, done) {
-                connection.query("SELECT * FROM libraries WHERE name = ?", [username], function (err, rows) {
-                    if (err)
-                        return done(err);
-                    if (rows.length) {
-                        return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
-                    } else {
-                        let newUserMysql = {
-                            name: username,
-                            password: bcrypt.hashSync(password, null, null),
-                            location: req.body.location,
+        Library.findByPk(id).then(function (user) {
+
+            if (user) {
+
+                done(null, user.get());
+
+            } else {
+
+                done(user.errors, null);
+
+            }
+
+        });
+
+    });
+    passport.use('local-signup', new LocalStrategy(
+        {
+            usernameField: 'name',
+            passwordField: 'password',
+            passReqToCallback: true
+        },
+
+        function (req, email, password, done) {
+
+            let generateHash = function (password) {
+
+                return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
+
+            };
+
+            Library.findOne({
+                where: {
+                    name: email
+                }
+            }).then(function (user) {
+
+                if (user) {
+
+                    return done(null, false, req.flash('signupMessage', 'That username is already taken.'))
+
+                } else {
+
+                    let userPassword = generateHash(password);
+
+                    let data =
+                        {
+                            name: email,
+                            password: userPassword,
+                            location: 'location',
                             createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
                             updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
-                            // use the generateHash function in our user model
+
                         };
 
-                        let insertQuery = "INSERT INTO libraries ( name, password, location,createdAt,updatedAt ) values (?,?,?,?,?)";
+                    Library.create(data).then(function (newUser, created) {
 
-                        connection.query(insertQuery, [newUserMysql.name, newUserMysql.password, newUserMysql.location, newUserMysql.createdAt, newUserMysql.updatedAt], function (err, rows) {
-                            console.log(err);
-                            console.log('!!!!',rows);
-                            newUserMysql.id = rows.insertId;
-                            return done(null, newUserMysql);
-                        });
-                    }
+                        if (!newUser) {
+
+                            return done(null, false);
+
+                        }
+
+                        if (newUser) {
+
+                            return done(null, newUser);
+
+                        }
+
+                    });
+
+                }
+
+            });
+
+        }
+    ));
+    passport.use('local-login', new LocalStrategy(
+        {
+
+            usernameField: 'name',
+            passwordField: 'password',
+            passReqToCallback: true
+
+        },
+
+
+        function (req, email, password, done) {
+            let isValidPassword = function (userpass, password) {
+
+                return bCrypt.compareSync(password, userpass);
+
+            }
+
+            Library.findOne({
+                where: {
+                    name: email
+                }
+            }).then(function (user) {
+
+                if (!user) {
+
+                    return done(null, false, req.flash('loginMessage', 'No user found.'));
+
+                }
+
+                if (!isValidPassword(user.password, password)) {
+
+                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+
+                }
+
+
+                let userinfo = user.get();
+                return done(null, userinfo);
+
+
+            }).catch(function (err) {
+
+                console.log("Error:", err);
+
+                return done(null, false, {
+                    message: 'Something went wrong with your Signin'
                 });
-            })
-    );
-
-
-    passport.use(
-        'local-login',
-        new LocalStrategy({
-                usernameField: 'name',
-                passwordField: 'password',
-                passReqToCallback: true
-            },
-            function (req, username, password, done) {
-                connection.query("SELECT * FROM libraries WHERE name = ?", [username], function (err, rows) {
-                    if (err)
-                        return done(err);
-                    if (!rows.length) {
-                        return done(null, false, req.flash('loginMessage', 'No user found.'));
-                    }
-
-                    // if the user is found but the password is wrong
-                    if (!bcrypt.compareSync(password, rows[0].password))
-                        return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
-
-                    // all is well, return successful user
-                    return done(null, rows[0]);
-                });
-            })
-    );
+            });
+        }
+    ));
 
 };
