4dbc62647b7f68b000c7725eb2f1ac6ffd901d8d
kirylmisiuk <kirill.misiuk@gmail.com>
2019-11-11 11:10:19 +0300

    restore

--- js/passport.js
+++ js/passport.js
@@ -3,29 +3,9 @@ const mysql = require('mysql');
 const bcrypt = require('bcrypt-nodejs');
 const dbconfig = require('../config/passportDB');
 
-let connection;
-connection = mysql.createConnection(dbconfig.connection);
-connection.query('USE ' + dbconfig.database);
-function handleDisconnect() {
-    connection = mysql.createConnection(dbconfig.connection);
-    connection.query('USE ' + dbconfig.database);
-    connection.connect(function(err) {
-        if(err) {
-            console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000);
-        }
-    });
 
-    connection.on('error', function(err) {
-        console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnect();
-        } else {
-            throw err;
-        }
-    });
-}
-handleDisconnect();
+const connection = mysql.createConnection(dbconfig.connection);
+connection.query('USE ' + dbconfig.database);
 
 
 module.exports = function (passport) {
